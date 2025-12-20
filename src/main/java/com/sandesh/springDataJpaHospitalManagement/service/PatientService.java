55775afe4bec07ecda7687347ee06e9bbf4d7f20
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2025-12-20 19:16:04 +0545

    Table Annotation, Unique constraints, and Indexes

--- src/main/java/com/sandesh/springDataJpaHospitalManagement/service/PatientService.java
+++ src/main/java/com/sandesh/springDataJpaHospitalManagement/service/PatientService.java
@@ -2,6 +2,7 @@ package com.sandesh.springDataJpaHospitalManagement.service;
 
 import com.sandesh.springDataJpaHospitalManagement.entity.Patient;
 import com.sandesh.springDataJpaHospitalManagement.repository.PatientRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,9 +16,14 @@ public class PatientService {
         this.patientRepository = patientRepository;
     }
 
+//    @Transactional
     public Patient getPatientID(Long id) {
         Patient p1 = patientRepository.findById(id).orElse(null);
         Patient p2 = patientRepository.findById(id).orElse(null);
+        System.out.println(p1 == p2);
+
+        p1.setAge(23); //this is managed by transactional and doesnot need to be saved
+//        patientRepository.save(p1);
         return p1;
     }
 
