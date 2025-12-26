5686882764b49dde59c4a67d387b5e2a9128a38f
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2025-12-26 18:37:50 +0545

    JPQL

--- src/test/java/com/sandesh/springDataJpaHospitalManagement/PatientTest.java
+++ src/test/java/com/sandesh/springDataJpaHospitalManagement/PatientTest.java
@@ -2,22 +2,16 @@ package com.sandesh.springDataJpaHospitalManagement;
 
 import com.sandesh.springDataJpaHospitalManagement.entity.Patient;
 import com.sandesh.springDataJpaHospitalManagement.repository.PatientRepository;
-import com.sandesh.springDataJpaHospitalManagement.service.PatientService;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @SpringBootTest
 @RequiredArgsConstructor
 public class PatientTest {
 
-    private final PatientService patientService;
+
     private final PatientRepository patientRepository;
 
-    public Patient getPatientName(String name){
-        Patient patientbyName = patientService.getPatientName(name);
-        return patientbyName;
-    }
 }
