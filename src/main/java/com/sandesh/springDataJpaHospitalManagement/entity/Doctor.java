807d893b2959cda59345c97abefeb5b937391c71
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2026-01-04 19:56:50 +0545

    JPA Buddy

---
+++ src/main/java/com/sandesh/springDataJpaHospitalManagement/entity/Doctor.java
@@ -0,0 +1,19 @@
+package com.sandesh.springDataJpaHospitalManagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "doctor_id")
+    private Long doctorId;
+
+    @Column(nullable = false)
+    private String doctorName;
+
+    @Column(nullable = false)
+    private String doctorEmail;
+
+    private String specialization;
+}
